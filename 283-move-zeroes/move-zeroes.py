6e56ff4c8d8ae58cbc789a1f61693b45829a230e
Victor Kipchumba <victorkipchumba039@gmail.com>
2026-01-22 07:28:26 +0300

    Time: 3 ms (89.24%) | Memory: 13.6 MB (42.66%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        write = 0
+        for read in range(len(nums)):
+            if nums[read] != 0:
+                nums[write] = nums[read]
+                write += 1
+        for i in range(write, len(nums)):
+            nums[i] = 0
